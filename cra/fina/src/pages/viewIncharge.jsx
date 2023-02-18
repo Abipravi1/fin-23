e46ebec2ceada97f98aa1b5247e8031e070be504
sathish5592 <114867663+sathish5592@users.noreply.github.com>
2023-02-18 20:40:06 +0530

    changes

--- cra/fina/src/pages/viewIncharge.jsx
+++ cra/fina/src/pages/viewIncharge.jsx
@@ -1,9 +1,35 @@
-import React, { useState, useEffect } from 'react';
+	import React, { useState, useEffect } from 'react';
 import { AxiosInstance } from '../axios/axios';
 import toast from 'react-hot-toast';
+import Button from 'react-bootstrap/Button';
+import TextField from '@mui/material/TextField';
 
 export default function ViewIncharge() {
 	const [data, setData] = useState([]);
+	const [showpage,setshowpage]=useState(false);
+	const [selectedrow,setselectedrow]=useState(null)
+	const [newData,setnewData]=useState([]);
+	const [inputValue,setInputValue]=useState('');
+	const selectedUserData=selectedrow?data.find(row=> row.id ===selectedrow):null
+    
+
+    const handleAddNewData=()=>{
+		if (inputValue !==''){
+			const date=new Date().toLocaleString();
+			setnewData(prevData=>[...prevData, {date, value:inputValue}])
+			setInputValue("")
+		}
+	}
+	const handleEdit=(index)=>{
+     const newValue=prompt("enter a new value",newData[index].value);
+	 if(newValue !== null){
+		setnewData(prevData=>{
+			const newDataCopy=[...prevData];
+			newDataCopy[index].value=newValue;
+			return newDataCopy;
+		})
+	 }
+	}
 
 	const callFunc = () => {
 		AxiosInstance.get('/incharge1/').then((res) => setData(res.data));
@@ -23,6 +49,7 @@ export default function ViewIncharge() {
 	};
 
 	return (
+
 		<div className='d-flex flex-column mt-3 p-3 gap-3 w-100'>
 			<div>
 				<a href='/incharge' className='btn btn-primary'>
@@ -37,20 +64,25 @@ export default function ViewIncharge() {
 							<th>Place</th>
 							<th>Contact</th>
 							<th>Amount</th>
-							<th>Action</th>
+							<th>Actions</th>
 						</tr>
 					</thead>
 					<tbody>
 						{data.length > 0 ? (
 							data?.map((details) => (
-								<tr>
+								<tr   onClick={()=> setselectedrow(details.id)}  >
 									<td>{details.name}</td>
 									<td>{details.place}</td>
 									<td>{details.contact}</td>
 									<td>{details.amount}</td>
 									<td>
 										<button
-											onClick={(e) => delete1(details.id)}
+											onClick={(e) => {
+												setshowpage(true)
+												delete1(details.id)
+											     console.log(details.name);                  }
+											                       }
+												
 											className='btn btn-danger'>
 											X
 										</button>
@@ -64,6 +96,59 @@ export default function ViewIncharge() {
 						)}
 					</tbody>
 				</table>
+
+			{showpage ?<div className='popup'>
+
+				<div  style={{height:"400px",width:"800px",position:"absolute",top:"150px", left:"300px",zIndex:"2", backgroundColor:"wheat"}} >
+				<Button variant="primary" style={{margin:"20px 20px"}} onClick={()=> setshowpage(false)} >close</Button>
+          
+                 <div>
+				<h6  style={{marginLeft:"20px",marginTop:"20px"}}  >{selectedUserData?.name}</h6>
+             <div>  <div style={{display:"flex",flexDirection:"column",gap:"20px"}}>
+				<TextField style={{width:"200px",marginLeft:"20px",marginTop:"80px"}}
+						id='outlined-basic'
+						
+						label='Amount'
+						size='small'
+						type='number'
+						className='form-control'
+						variant='outlined'
+					/>
+
+                <Button onClick={handleAddNewData} style={{width:"200px",marginTop:"20px",marginLeft:"20px"}} variant="success">Save</Button>
+				</div>
+				<div>
+					<table>
+						<thead>
+							<tr>
+								<th>Date</th>
+								<th>Value</th>
+								<th>Action</th>
+							</tr>
+						</thead>
+						<tbody>
+                          {newData.map((row,index)=>{
+							<tr key={index}>
+                              <td>{row.date}</td>
+							  <td>{row.value}</td>
+							  <td><button onClick={()=> handleEdit(index)}  >edit</button></td>
+
+							</tr>
+						  })}
+                             
+
+						</tbody>
+					</table>
+				</div>
+				</div>
+               
+				 </div>
+
+				</div>
+			</div>:<></> }	
+               
+
+				
 			</div>
 		</div>
 	);
